7161c48672df6f90d2ded5647523442060e510b4
Andrew Skillett <andrewskillett@gmail.com>
2022-08-08 16:58:38 -0700

    6

--- 6kyu/js/main.js
+++ 6kyu/js/main.js
@@ -67,6 +67,26 @@ let multiplicationTable = function(size){
     }
     return mainArr;
 }
+/*
+5. 
+A pangram is a sentence that contains every single letter of the alphabet at least once. 
+For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses 
+the letters A-Z at least once (case is irrelevant).
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+ */
+function isPangram(string){
+    string = string.toLowerCase()
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let alphaArr = alphabet.split('');
+    let arr = string.split('');
+    arr.forEach((elem) => {
+        if(alphaArr.includes(elem)){
+            let index = alphaArr.indexOf(elem);
+            alphaArr.splice(index,1);
+        }
+    });
+    return alphaArr.length === 0 ? true : false;
+}
 
 
 
