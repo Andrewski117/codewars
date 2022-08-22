d42d717a858187fd79b0fb9ce201e92dcacc69e1
Andrew Skillett <andrewskillett@gmail.com>
2022-08-21 18:01:53 -0700

    another 7

--- 7kyu/js/main.js
+++ 7kyu/js/main.js
@@ -109,6 +109,30 @@ var greet = function(name) {
     return `Hello ${final}!`
 };
 
+/*
+9.
+Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer. Square all numbers k (0 <= k <= n) between 0 and n.
+Count the numbers of digits d used in the writing of all the k**2. Call nb_dig (or nbDig or ...) the function taking n and d 
+as parameters and returning this count.
+ */
+function nbDig(n, d) {
+    let ar = [];
+    let strD = d.toString();
+    let counter = 0;
+    for(let i = 0; i <= n; i++){
+        ar.push(i*i);
+    }
+    ar.forEach((elem) => {
+        let temp = elem.toString();
+        for(let i = 0; i < temp.length; i++){
+            if(temp[i] === strD){
+                counter++;
+            }
+        }
+    });
+    return counter;
+}
+
 
 
 
