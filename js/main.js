97b182d51b991b61573f81e8432573aac18a4339
Andrew Skillett <andrewskillett@gmail.com>
2022-09-12 11:43:32 -0700

    boring 8 kyu

--- js/main.js
+++ js/main.js
@@ -87,6 +87,19 @@ function guessBlue(blueStart, redStart, bluePulled, redPulled) {
     let remaining = (blueStart - bluePulled) + (redStart -redPulled);
     return (blueStart -bluePulled) / remaining;
 }
+/*
+8.
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+ */
+function sumOfDifferences(arr) {
+    if(arr.length <=1 ) return 0;
+    arr = arr.sort((a,b) => a-b).reverse();
+    let sum = 0;
+    for(let i = 0; i < arr.length-1; i++){
+        sum += (arr[i] - arr[i+1]);
+    }
+    return sum;
+}
 
 
 
